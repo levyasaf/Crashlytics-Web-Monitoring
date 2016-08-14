d6eaebec9bfb18b508bc9d5b4f155aba891d31d3
Asaf Levy <asaf@sears.co.il>
2016-08-14 16:01:30 +0200

    Changed log format

--- app/common.py
+++ app/common.py
@@ -1,12 +1,14 @@
 import os
 import json
+import datetime
 
 def process_data(data):
     dict_obj = json.loads(data)
-   
-    #print dict_obj["payload"]["app"]["name"]
-
-    if dict_obj["event"] == "issue_impact_change":
-        with open("/tmp/" + dict_obj["payload"]["app"]["name"]  + ".log", "a+") as myfile:
-            myfile.write(data + "\n")
+    now = datetime.datetime.now()   
+    event_type = dict_obj["event"]
+    app_name = dict_obj["payload"]["app"]["name"]
+    
+    if event_type == "issue_impact_change":
+        with open("/tmp/" + app_name  + ".log", "a+") as myfile:
+            myfile.write(now.strftime("%Y-%m-%d %H:%M") + ";" + app_name + ";" + data + "\n")
     return 
