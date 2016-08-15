28c51a01b9a07194d1378676b29ab427de0a786e
Asaf Levy <asaf@sears.co.il>
2016-08-15 16:50:33 +0200

    add post to sears service

--- app/common.py
+++ app/common.py
@@ -1,6 +1,7 @@
 import os
 import json
 import datetime
+import requests
 
 def process_data(data):
     dict_obj = json.loads(data)
@@ -11,4 +12,5 @@ def process_data(data):
     if event_type == "issue_impact_change":
         with open("/tmp/" + app_name  + ".log", "a+") as myfile:
             myfile.write(now.strftime("%Y-%m-%d %H:%M") + ";" + app_name + ";" + data + "\n")
+    r = requests.post("http://sears-searsi.rhcloud.com/crashlytics", data=json.dumps(data))
     return 
